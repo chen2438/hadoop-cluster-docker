e6bf2f2b7c900b38b3a48ee315e738b0d5a6c54c
陈昊天 <2363822647@qq.com>
2024-04-07 23:01:27 +0800

    fix: map DataNodes port & change NameNode RPC port

--- start-container.sh
+++ start-container.sh
@@ -10,7 +10,7 @@ sudo docker run -itd \
 	--net=hadoop \
 	-p 50070:50070 \
 	-p 8088:8088 \
-	-p 9000:9000 \
+	-p 8020:8020 \
 	--name hadoop-master \
 	--hostname hadoop-master \
 	chen2438/hadoop:jdk8 &>/dev/null
@@ -22,6 +22,7 @@ while [ $i -lt $N ]; do
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
 		--net=hadoop \
+		-p 50010:50010 \
 		--name hadoop-slave$i \
 		--hostname hadoop-slave$i \
 		chen2438/hadoop:jdk8 &>/dev/null
